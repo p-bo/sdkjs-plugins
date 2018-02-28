e78dec792a1d48f42544113bb099d482e7b306eb
Alexey Matveev <Alexey.Matveev@onlyoffice.com>
2018-02-28 18:34:26 +0300

    [Html parse] Create parse to retrieve data

--- htmlparser/code.js
+++ htmlparser/code.js
@@ -43,17 +43,10 @@
                 return;
             }
 
-			//if request sucsess
-			if (false)
-			{
-				window.Asc.plugin.resizeWindow(620, 480, 390, 400, 0, 0);
-			}
 		};
 
-		if (_url == "")
+		if (_url != "")
 		{
-			this.resizeWindow(350, 90, 350, 90, 350, 90);
-		}else{
 			document.getElementById("textbox_url").value = _url;
 			document.getElementById("textbox_button").onclick();
 		}
@@ -68,9 +61,9 @@
 				async: true,
 				url: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20csv%20where%20url%3D'http%3A%2F%2Freader.elisdn.ru%2F%3Furl%3D"+_url+"'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",
 				success: function(data){
-					//console.log(JSON.stringify(data.query.results.row));
-					parse_data(data.query.results.row);
-					
+					//console.log(data);
+					change_plugin();
+					//parse_data(data.query.results.row);
 				},
 				error: function(err){
 					alert("err data req");
@@ -81,24 +74,47 @@
 	};
 
 	function parse_data(data){
-		var new_data='';
-		for (var i=0; i<data.length;i++){
-			new_data+=JSON.stringify(data[i]);
-		}
-		var par_1 = '\"col',
-		par_2 = '":';
-		var temp;
-		var pos = 0;
+		var new_data='',
+			count = 1;
+			pos = 0,
+			tables = {};
+		for (let i=0;i<data.length;i++)
+			for (let key in data[i])
+				new_data += data[i][key];
 		while (true) {
-			var foundPos_1 = new_data.indexOf(par_1, pos);
-			var foundPos_2 = new_data.indexOf(par_2, foundPos_1);
-			if (foundPos_1 == -1) break;
-			temp = new_data.substring((foundPos_1),(foundPos_2+2));
-			//pos = foundPos_1 + 1;
-			new_data = new_data.replace( new RegExp(temp, 'g'),"");
+			var foundPos_1 = new_data.indexOf("<td class='number'>", 0);
+			var foundPos_2 = new_data.indexOf("</td>", foundPos_1);
+			if ((foundPos_1 == -1) || (foundPos_2 ==-1)) break;
+			data = new_data.substring((foundPos_1),(foundPos_2+5));
+			new_data = new_data.replace( new RegExp(data, 'g'),"");
 		}
+		// document.body.innerHTML = new_data;			//where past
+		// new_data = document.body.innerText;
+		// document.body.innerHTML ='';
+		while (true) {
+			var foundPos_1 = new_data.indexOf("<table", pos);
+			var foundPos_2 = new_data.indexOf("</table>", foundPos_1);
+			if ((foundPos_1 == -1) || (foundPos_2 ==-1)) break;
+			pos = 'table_'+ count;
+			tables[pos] = new_data.substring((foundPos_1),(foundPos_2+8));
+			pos = foundPos_1 + 1;
+			count++;
+		}
+		console.log(tables);
+		return tables;
+	};
+
+	function change_plugin(){
 		
-		console.log(new_data);
+		var combo = document.createElement('combo'),
+			div = document.createElement('div');
+		combo.id = 'tabla_list';
+		combo.className = 'combo';
+		div.id = 'data_div';
+		div.className = 'data_div';
+
+		//document.getElementById('body').appendChild(combo);
+		//document.getElementById('body').appendChild(div);
 	};
 
 	function cancelEvent(e){
@@ -115,15 +131,16 @@
 	{
 		if (id == 0)
 		{
-	        var url = document.getElementById("textbox_url").value;
+	       // var url = document.getElementById("textbox_url").value;
 
 			//if request failed
-            if (false)
+            if (true)
             {
                 document.getElementById("textbox_url").style.borderColor = "#d9534f";
                 document.getElementById("input_error_id").style.display = "block";
                 return;
-            }
+			}
+			this.executeCommand("close", "");
 		}
 		else
 		{
